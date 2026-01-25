0efafac480cbd477a7bc96f893e6402c85d7ce8c
passion348 <jeny.girich@gmail.com>
2026-01-25 20:48:27 +0000

    exercise1.1

--- src/Quotation.java
+++ src/Quotation.java
@@ -1,5 +1,5 @@
 public class Quotation {
-  String quote = "The only true wisdom is knowing that you know nothing!";
+  String quote = "The only true wisdom is knowing that u know nothing!";
   public void display() {
     System.out.println(quote);
   }
